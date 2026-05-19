588605d19c0afdcf53c854e888962505525e2c15
Aishwarya <aishwarya@Aishwaryas-MacBook-Air.local>
2026-05-19 20:28:31 +0530

    Commented code for deployment

--- dashboard.py
+++ dashboard.py
@@ -1,102 +1,107 @@
-from main import run_trades
+# from main import run_trades
 import streamlit as st
-import alpaca_trade_api as tradeapi
-from config import API_KEY, SECRET_KEY, BASE_URL
-import pandas as pd
-import time
-from strategy_bot import data_indicators, get_strategy_data
-from data_collector import get_price_data
 
-# from main import get_main_data
+st.title("My Trading Bot")
+st.success("Deployment Successful")
 
-API_KEY = API_KEY
-SECRET_KEY = SECRET_KEY
-BASE_URL = BASE_URL
+# import streamlit as st
+# import alpaca_trade_api as tradeapi
+# from config import API_KEY, SECRET_KEY, BASE_URL
+# import pandas as pd
+# import time
+# from strategy_bot import data_indicators, get_strategy_data
+# from data_collector import get_price_data
 
-try:
-    api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version="v2")
-except Exception as e:
-    st.error(e)
+# # from main import get_main_data
 
-st.title("Trading Bot Dashboard")
+# API_KEY = API_KEY
+# SECRET_KEY = SECRET_KEY
+# BASE_URL = BASE_URL
 
-if st.button("Run strategy now"):
-    print("Running strategy...")
-    trade_history = run_trades()
-    st.dataframe(pd.DataFrame(trade_history))
+# try:
+#     api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version="v2")
+# except Exception as e:
+#     st.error(e)
 
-account = api.get_account()
+# st.title("Trading Bot Dashboard")
 
-st.subheader("Account Info")
-st.write("Equity:", account.equity)
-st.write("Buying Power:", account.buying_power)
+# if st.button("Run strategy now"):
+#     print("Running strategy...")
+#     trade_history = run_trades()
+#     st.dataframe(pd.DataFrame(trade_history))
 
-# rsiValue = improved_strategy()
-# st.subheader("RSI Values")
-# st.write(rsiValue[['Close', 'RSI']].tail(10))fs
-# st.line_chart(rsiValue['RSI'])
+# account = api.get_account()
 
-positions = api.list_positions()
+# st.subheader("Account Info")
+# st.write("Equity:", account.equity)
+# st.write("Buying Power:", account.buying_power)
 
-st.subheader("Open Positions")
+# # rsiValue = improved_strategy()
+# # st.subheader("RSI Values")
+# # st.write(rsiValue[['Close', 'RSI']].tail(10))fs
+# # st.line_chart(rsiValue['RSI'])
 
-for position in positions:
-    st.write("Symbol:", position.symbol, "Quantity:", position.qty, "Current Price:", position.current_price)
+# positions = api.list_positions()
 
-data = []
+# st.subheader("Open Positions")
 
-orders = api.list_orders(status='closed', limit=20)
+# for position in positions:
+#     st.write("Symbol:", position.symbol, "Quantity:", position.qty, "Current Price:", position.current_price)
 
-for order in orders:
-    data.append({
-        "Symbol": order.symbol,
-        "Side": order.side,
-        "Qty": order.qty,
-        "Status": order.status,
-        "Entry Price": float(position.avg_entry_price),
-        "Current Price": float(position.current_price),
-        "Unrealized P&L": float(position.unrealized_pl)
-    })
+# data = []
 
-df = pd.DataFrame(data)
-st.dataframe(df)
+# orders = api.list_orders(status='closed', limit=20)
 
-data_for_chart = get_price_data("AAPL")
-stock_data = data_indicators(data_for_chart)
+# for order in orders:
+#     data.append({
+#         "Symbol": order.symbol,
+#         "Side": order.side,
+#         "Qty": order.qty,
+#         "Status": order.status,
+#         "Entry Price": float(position.avg_entry_price),
+#         "Current Price": float(position.current_price),
+#         "Unrealized P&L": float(position.unrealized_pl)
+#     })
 
-st.line_chart(stock_data[['short_ma', 'long_ma', 'ma200']])
-st.line_chart(stock_data['rsi'])
+# df = pd.DataFrame(data)
+# st.dataframe(df)
 
-# st.subheader("Trade History")
-# st.subheader("Open Positions with P&L")
+# data_for_chart = get_price_data("AAPL")
+# stock_data = data_indicators(data_for_chart)
+
+# st.line_chart(stock_data[['short_ma', 'long_ma', 'ma200']])
+# st.line_chart(stock_data['rsi'])
+
+# # st.subheader("Trade History")
+# # st.subheader("Open Positions with P&L")
 
 
-total_pl = sum(float(p.unrealized_pl) for p in positions)
+# total_pl = sum(float(p.unrealized_pl) for p in positions)
 
-# st.subheader("Total Unrealized P&L")
+# # st.subheader("Total Unrealized P&L")
 
-# if total_pl > 0:
-#     st.success(f"Profit: ${total_pl:.2f}")
+# # if total_pl > 0:
+# #     st.success(f"Profit: ${total_pl:.2f}")
+# # else:
+# #     st.error(f"Loss: ${total_pl:.2f}")
+
+# equity = float(account.equity)
+# last_equity = float(account.last_equity)
+
+# daily_pl = equity - last_equity
+
+# st.subheader("Daily P&L")
+
+# if daily_pl > 0:
+#     st.success(f"Today's Profit: ${daily_pl:.2f}")
 # else:
-#     st.error(f"Loss: ${total_pl:.2f}")
+#     st.error(f"Today's Loss: ${daily_pl:.2f}")
 
-equity = float(account.equity)
-last_equity = float(account.last_equity)
+# pl_data = pd.DataFrame({
+#     "P&L": [total_pl]
+# })
 
-daily_pl = equity - last_equity
+# # st.bar_chart(pl_data)
 
-st.subheader("Daily P&L")
-
-if daily_pl > 0:
-    st.success(f"Today's Profit: ${daily_pl:.2f}")
-else:
-    st.error(f"Today's Loss: ${daily_pl:.2f}")
-
-pl_data = pd.DataFrame({
-    "P&L": [total_pl]
-})
-
-# st.bar_chart(pl_data)
-
-time.sleep(60)
-st.rerun()
+# time.sleep(60)
+# st.rerun()
